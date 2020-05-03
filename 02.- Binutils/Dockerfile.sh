2bbf10e7990485b07c2c1eacbcba6a0a7c0671c1
Javier Moreno <jgmore@gmail.com>
2020-05-03 14:29:28 +0200

    Added binutils layer

---
+++ 02.- Binutils/Dockerfile.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+# Download and extract
+wget http://ftp.rediris.es/mirror/GNU/binutils/binutils-2.31.tar.gz
+tar xf binutils-2.31.tar.gz
+
+# Build
+mkdir build-binutils
+cd build-binutils
+
+
+# Set configuration ()
+OPTIONS=()
+
+# Set install prefix
+OPTIONS+=(--prefix=$CROSS_COMPILER_PREFIX)
+
+# Disable -Werror in bootstrap stage2 and later
+OPTIONS+=(--disable-werror)
+
+# Configure for building compilers for TARGET [HOST]
+OPTIONS+=(--target=$CONFIG_CROSS_TARGET)
+
+# Do not use Native Language Support
+OPTIONS+=(--disable-nls)
+
+# not valid
+#OPTIONS+=(--with-sysroot)
+#OPTIONS+=(--with-arch=$ARM_VERSION)
+#OPTIONS+=(--with-float=$ARM_FLOAT_TYPE)
+#OPTIONS+=(-with-fpu=vfp)
+
+
+# Build steps
+../binutils-2.31/configure ${OPTIONS[@]}
+make -j$(nproc --all)
+make install
