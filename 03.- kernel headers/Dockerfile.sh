7999294e6505a47d9ad2ba300f54bcde128742cc
Javier Moreno <jgmore@gmail.com>
2020-05-03 17:27:01 +0200

    Added kerner header layer

---
+++ 03.- kernel headers/Dockerfile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Download and extract kernel source
+wget https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/linux-4.8.16.tar.xz
+tar -xf linux-4.8.16.tar.xz
+
+
+# Build kernel headers
+cd linux-4.8.16
+make ARCH=$MAKE_ARCH INSTALL_HDR_PATH=$CROSS_SYSROOT headers_install
